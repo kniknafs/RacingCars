3a57c33ba596b1c7d1d098097d694385d52cfbb7
Faradars <An@faradars.com>
2022-02-04 06:31:56 +0330

    New method

--- Main/src/com/faradars/entity/Mazda.java
+++ Main/src/com/faradars/entity/Mazda.java
@@ -3,11 +3,13 @@ package com.faradars.entity;
 public class Mazda implements ICar {
     @Override
     public String getName() {
-        return "Mazda";
+        return "Mazda Ali";
     }
 
     @Override
-    public int getSpeed() {
-        return 0;
+    public int getSpeed() { return 0; }
+
+    public void sample(){
+        int i=1;
     }
 }
