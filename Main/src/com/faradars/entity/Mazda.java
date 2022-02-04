cc2a8fd690097c59eb9fc1137d248fcd8ec5725d
Faradars <An@faradars.com>
2022-02-04 06:15:09 +0330

    Adding Mazda

---
+++ Main/src/com/faradars/entity/Mazda.java
@@ -0,0 +1,13 @@
+package com.faradars.entity;
+
+public class Mazda implements ICar {
+    @Override
+    public String getName() {
+        return "Mazda";
+    }
+
+    @Override
+    public int getSpeed() {
+        return 0;
+    }
+}
